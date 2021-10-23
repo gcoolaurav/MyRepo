d147779c02a6434370fbb97a00f8224008bcc0b1
gcoolaurav <59495893+gcoolaurav@users.noreply.github.com>
2021-10-23 11:11:24 +0530

    Result test

---
+++ WebApplication9/result.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="result.aspx.cs" Inherits="jquery_jquery_ajax_load_result" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    </form>
+</body>
+</html>
---
+++ WebApplication9/result.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class jquery_jquery_ajax_load_result : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string name = Request.Form["name"];
+            string city = Request.Form["city"];
+
+            Response.Write("Welcome Mr. " + name + " from " + city);
+            Response.End();
+        }
+    }
+}
